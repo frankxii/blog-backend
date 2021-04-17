cff0c3ff700a1761c2ff09be0df72db7a250e506
frank <frankw.wang@qq.com>
2021-04-18 02:25:31 +0800

    修改 博客文章model，新增分类model

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,16 @@ from django.db import models
 
 
 class Article(models.Model):
-    title = models.TextField()
-    body = models.TextField()
+    """博客文章表"""
+    title = models.TextField('标题')
+    body = models.TextField('内容')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, db_constraint=False)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    """分类"""
+    name = models.TextField('分类名称')
+    # create_time = models.DateTimeField(auto_now_add=True)
+    # update_time = models.DateTimeField(auto_now=True)

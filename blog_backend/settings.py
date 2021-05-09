5673b380ba5cbd7c9a5ffbfae55560014180319e
frank <frankw.wang@qq.com>
2021-05-09 20:08:29 +0800

    修改 添加allow hosts

--- blog_backend/settings.py
+++ blog_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ENV['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.frankxii.com', 'frankxii.com']
 
 # Application definition
 

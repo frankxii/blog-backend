6edd3e9318ac92848ae714c9880e69362205adcd
frank <frankw.wang@qq.com>
2021-05-18 15:10:30 +0800

    修复 redis断连问题，设置redis配置 health_check_interval=120

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,11 +1,11 @@
 import json
-from redis import Redis
+from redis import StrictRedis
 
 with open('blog_backend/env.json') as env:
     env_dict: dict = json.load(env)
     config: dict = env_dict['REDIS_INFO']
 
-redis = Redis(**config, decode_responses=True)
+redis = StrictRedis(**config, decode_responses=True, health_check_interval=120)
 
 with open('blog/Authority.json', 'rb') as authority_file:
     authority_config: list[dict] = json.load(authority_file)
